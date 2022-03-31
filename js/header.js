d5f76d0c1c12412a8876cb70abbbb27ea2e426b8
xsth <102748800+xsth@users.noreply.github.com>
2022-03-31 23:14:28 +0800

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,4 @@
+﻿document.write("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1\">");
+document.write("<link rel=\"stylesheet\" type=\"text\/css\" href=\"style.css\">");
+document.write("<link rel=\"icon\" type=\"image\/x-icon\" href=\"favicon.ico\">");
+document.write("<div id=\"header\"><a class=\"logo\" href=\"index.html\"><span class=\"b\">N<\/span><span class=\"r\">o<\/span><span class=\"y\">t<\/span><span class=\"g\">e<\/span><\/a><ul id=\"nav\"><li><a href=\"index.html\">首页<\/a><\/li><li><a href=\"motto.html\">格言<\/a><\/li><li><a href=\"bookmark.html\">书签<\/a><\/li><li><a href=\"data.html\">数据<\/a><\/li><li><a href=\"movies.html\">观影<\/a><\/li><\/ul><div id=\"sideMenu\"><span class=\"iconfont menu\" style=\"font-size: 24px\"><\/span><\/div><div id=\"sideMenuContainer\"><a href=\"index.html\">首页<\/a><a href=\"motto.html\">格言<\/a><a href=\"bookmark.html\">书签<\/a><a href=\"data.html\">数据<\/a><a href=\"movies.html\">观影<\/a><\/div><\/div>");
