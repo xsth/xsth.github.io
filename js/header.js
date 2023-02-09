b29cf10f08dd404163b09db15069465a9e191026
xsth <102748800+xsth@users.noreply.github.com>
2023-02-09 20:45:59 +0800

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 ﻿document.write("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1\">");
 document.write("<link rel=\"stylesheet\" type=\"text\/css\" href=\"style.css\">");
 document.write("<link rel=\"icon\" type=\"image\/x-icon\" href=\"favicon.ico\">");
-document.write("<script src=\"https:\/\/fastly.jsdelivr.net\/npm\/jquery@3.2.1\/dist\/jquery.min.js\"><\/script>")div><\/div>");
+document.write("<script src=\"https:\/\/fastly.jsdelivr.net\/npm\/jquery@3.2.1\/dist\/jquery.min.js\"><\/script>");
 document.write("<div id=\"header\"><a class=\"logo\" href=\"index.html\"><span class=\"b\">N<\/span><span class=\"r\">o<\/span><span class=\"y\">t<\/span><span class=\"g\">e<\/span><\/a><ul id=\"nav\"><li><a href=\"index.html\">首页<\/a><\/li><li><a href=\"motto.html\">格言<\/a><\/li><li><a href=\"bookmark.html\">书签<\/a><\/li><li><a href=\"movies.html\">观影<\/a><\/li><\/ul><div id=\"sideMenu\"><\/div><div id=\"sideMenuContainer\"><a href=\"bookmark.html\">书签<\/a><\/div><\/div>");
